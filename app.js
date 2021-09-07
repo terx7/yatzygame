8c585ab78637c246e10872576bef9018ba44fc3b
terx7 <erkileemet10@gmail.com>
2021-09-07 15:48:12 +0300

    added isMarked attribute and rollCounter

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 let roll = document.getElementById('roll')
-const dice = document.querySelectorAll('dice')
+const dice = document.querySelectorAll('.dice')
+let rollCount = 3
 console.log(dice)
+
+
+
+
+function reset() {
+    rollCount = 3
+}
+
+function saveDice(el) {
+    if (el.getAttribute('isMarked')) {
+        el.style.border = '3px solid black'
+        el.removeAttribute('isMarked')
+    } else {
+        el.style.border = '3px solid red'
+        el.setAttribute('isMarked', true)
+    }
+
+
+
+}
+
 function rollDice() {
     num = Math.floor(Math.random() * 6) + 1
     // if (num == 0) {
@@ -9,23 +31,36 @@ function rollDice() {
     return num;
 }
 
-document.getElementById("dice1").innerHTML = rollDice();
-document.getElementById("dice2").innerHTML = rollDice();
-document.getElementById("dice3").innerHTML = rollDice();
-document.getElementById("dice4").innerHTML = rollDice();
-document.getElementById("dice5").innerHTML = rollDice();
+// document.getElementById("dice1").innerHTML = rollDice();
+// document.getElementById("dice2").innerHTML = rollDice();
+// document.getElementById("dice3").innerHTML = rollDice();
+// document.getElementById("dice4").innerHTML = rollDice();
+// document.getElementById("dice5").innerHTML = rollDice();
 
 function rollAllDice() {
     console.log('yes')
 
+    if (rollCount >= 0) {
+        console.log(rollCount)
+        rollCount--
 
-    document.getElementById("dice1").innerHTML = rollDice();
-    document.getElementById("dice2").innerHTML = rollDice();
-    document.getElementById("dice3").innerHTML = rollDice();
-    document.getElementById("dice4").innerHTML = rollDice();
-    document.getElementById("dice5").innerHTML = rollDice();
+        dice.forEach(el => {
+            console.log(el.innerHTML)
+            if(!el.getAttribute('isMarked')){
+                el.innerHTML = rollDice()
+            }
+        });
+
+    } else {
+        console.log('You cant roll anymore!')
+    }
+
+
+
 }
 
-dice.children.forEach(el => {
-    console.log(el)
-});
+// dice.forEach(el => {
+//     console.log(el.innerText)
+// });
+
+rollAllDice();
