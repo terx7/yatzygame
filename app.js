7d75cd0a54f0d8a1f6445360301d7e1e7f2057e9
terx7 <erkileemet10@gmail.com>
2021-09-08 11:46:01 +0300

    added small score table and improved reset func

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ let rollCount = 3
 // console.log(dice)
 
 diceDict = {
-    dice1 : {
-        value : 0,
-        isLocked : false,
+    dice1: {
+        value: 0,
+        isLocked: false,
     },
-    dice2 : {
-        value : 0,
-        isLocked : false
+    dice2: {
+        value: 0,
+        isLocked: false
     },
-    dice3 : {
-        value : 0,
-        isLocked : false
+    dice3: {
+        value: 0,
+        isLocked: false
     },
-    dice4 : {
-        value : 0,
-        isLocked : false
+    dice4: {
+        value: 0,
+        isLocked: false
     },
-    dice5 : {
-        value : 0,
-        isLocked : false
+    dice5: {
+        value: 0,
+        isLocked: false
     },
 }
 // console.log(diceDict);
@@ -31,16 +31,20 @@ diceDict = {
 
 function reset() {
     rollCount = 3
+    dice.forEach(el => {
+        el.style.border = '3px solid black'
+        diceDict[el.id].isLocked = false;
+    })
 }
 
 function saveDice(el) {
     console.log(el)
-    if (diceDict[el.id].isLocked) {
-        document.getElementById(el.id).style.border = '3px solid black'
-        diceDict[el.id].isLocked = false;
+    if (diceDict[el].isLocked) {
+        document.getElementById(el).style.border = '3px solid black'
+        diceDict[el].isLocked = false;
     } else {
-        document.getElementById(el.id).style.border = '3px solid red'
-        diceDict[el.id].isLocked = true;
+        document.getElementById(el).style.border = '3px solid red'
+        diceDict[el].isLocked = true;
     }
 
 
@@ -68,8 +72,8 @@ function rollAllDice() {
         console.log(rollCount)
         rollCount--
 
-        for (let el in diceDict){
-            if(!diceDict[el].isLocked){
+        for (let el in diceDict) {
+            if (!diceDict[el].isLocked) {
                 diceDict[el].value = rollDice()
                 document.getElementById(el).innerHTML = diceDict[el].value
             }
